3f15e85f18cec55bf154c240b0f4326c7de99a41
Richard Boldway <richard.boldway@gmail.com>
2017-02-26 18:56:14 -0600

    4.3 Async actions with redux-thunk

--- Ch04/04_03/start/src/index.js
+++ Ch04/04_03/start/src/index.js
@@ -1,5 +1,12 @@
 import storeFactory from './store'
+import { randomGoals } from './actions'
 
 const store = storeFactory()
 
+store.dispatch(
+    randomGoals()
+)
 
+store.dispatch(
+    randomGoals()
+)
